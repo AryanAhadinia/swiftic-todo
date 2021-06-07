724be83a83bd58528b19d2879ae87ee3354df4fb
Shahab HM <59169907+shahabhm@users.noreply.github.com>
2021-06-07 21:42:27 +0430

    task class

--- main.swift
+++ main.swift
@@ -16,3 +16,38 @@ class Category : CustomStringConvertible {
 	    return "\(name) with \(tasks.count) tasks"
 	}
 }
+
+class Task : Equatable, CustomStringConvertible  {
+	static var idGenerator = 0
+
+	var id: Int
+	var title: String
+	var content: String
+	var priority: Int
+	var creationDate: Date
+	var category: Category?
+
+	public init(title: String, content: String, priority: Int) {
+		self.id = Task.idGenerator
+		self.title = title
+		self.content = content
+		self.priority = priority
+		self.creationDate = Date()
+		self.category = nil
+		Task.idGenerator += 1
+	}
+
+	public var description: String {
+		let dateformat = DateFormatter()
+        dateformat.dateFormat = "EEEE, MMM d, yyyy"
+		dateformat.timeZone = TimeZone.current
+        let formattedDateDay = dateformat.string(from: creationDate)
+		dateformat.dateFormat = "HH:mm:ss"
+		let formattedDateHour = dateformat.string(from: creationDate)
+		return "\"\(title)\": \"\(content)\" with priority \(priority) created on \(formattedDateDay) at \(formattedDateHour)"
+	}
+
+	public static func == (o1: Task, o2: Task) -> Bool{
+        return o1.id == o2.id
+    }
+}
